5d86a55c080f3e81900a22e25770b5d6ce174944
micaellopes1 <cacarochajp@hotmail.com>
2016-11-14 21:55:44 -0200

    Update AppTest.java

--- src/test/java/br/unipe/eng_soft/AppTest.java
+++ src/test/java/br/unipe/eng_soft/AppTest.java
@@ -6,7 +6,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App.wqeqweqwewq
  */
 public class AppTest 
     extends TestCase
